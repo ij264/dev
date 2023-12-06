8601c626e0c7cdbfb98eadbb16b711eec479ce2c
Issac Jacob <67367495+ij264@users.noreply.github.com>
2023-12-06 12:17:02 +0000

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from kernel import Kernel
-root = 'lyness/'
-target_dir = 'OUTPUT_const_visc_100'
-graph_types = ['geoid']
-degrees = [2, 5, 10]
-
-
-Ker = Kernel(target_dir=target_dir, graph_types=graph_types, degrees=degrees, root=root)
-Ker.plot()
